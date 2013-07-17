0d3a9d5062aa1da10e6cc807a34dde0eff46a99f
micah <michaeleschbacher.com>
2013-07-17 13:11:16 -0500

    Created a constructor and render methods for the property class

--- src/Property.php
+++ src/Property.php
@@ -6,8 +6,34 @@ class Property
 	protected $title;
 	protected $value;
 	protected $attributes = array();
-	public function __construct($title)
+	public function __construct($title, $value = null, $attributes = array())
 	{
 		$this->title = $title;
+		$this->value = $value;
+		$this->attributes = $attributes;
+	}
+
+	public function render()
+	{
+		if ($this->value) {
+			return sprintf(
+				'<%s%s>%s</%s>', 
+				$this->title, 
+				$this->renderAttributes(), 
+				$this->value, 
+				$this->title
+			);
+		}
+		return sprintf('<%s%s />', $this->title, $this->renderAttributes());		
+	}
+
+	protected function renderAttributes()
+	{
+		$ret = '';
+		$glue = ' ';
+		foreach ($this->attributes as $k=>$p) {
+			$ret.= sprintf('%s%s="%s"', $glue, $k, $p);
+		}		
+		return $ret;
 	}
 }
--- tests/PropertyTest.php
+++ tests/PropertyTest.php
@@ -13,7 +13,48 @@ class PropertyTest extends PHPUnit_Framework_TestCase
 		$this->assertTrue(is_array($obj->attributes));
 		$this->assertEquals(0, count($obj->attributes));
 	}
-	
+	public function testConstructorHasOptionalParams()
+	{
+		$obj = new MockProperty('title', 'value', array('a'=>'b'));
+		$this->assertInstanceOf('exemel\Property', $obj);
+		$this->assertEquals('title', $obj->title);
+		$this->assertEquals('value', $obj->value);
+		$this->assertTrue(is_array($obj->attributes));
+		$this->assertEquals(array('a'=>'b'), $obj->attributes);
+	}
+
+	public function testRenderWithValueHasAttributes()
+	{
+		$obj = new xml\Property('apple','pie',array('granny'=>'smith'));
+		$this->assertEquals('<apple granny="smith">pie</apple>', $obj->render());
+	}	
+	public function testRenderWithValueNoAttributes()
+	{
+		$obj = new xml\Property('apple','pie');
+		$this->assertEquals('<apple>pie</apple>', $obj->render());
+	}
+	public function testRenderNoValueHasAttributes()
+	{
+		$obj = new xml\Property('apple', null, array('prop'=>'val'));
+		$this->assertEquals('<apple prop="val" />', $obj->render());
+	}
+	public function testRenderNoValueNoAttributes()
+	{
+		$obj = new xml\Property('apple');
+		$this->assertEquals("<apple />", $obj->render());
+	}
+
+	public function testRenderAttributesWith()
+	{
+		$obj = new MockProperty('id',null, array('id1'=>'value', 'id2'=>'value2'));
+		$this->assertEquals(' id1="value" id2="value2"', $obj->fakeAttributes());
+	}
+	public function testRenderAttributesWithOut()
+	{
+		$obj = new MockProperty('id');
+		$this->assertEquals('', $obj->fakeAttributes());
+	}
+
 }
 
 class MockProperty extends xml\Property
@@ -21,6 +62,9 @@ class MockProperty extends xml\Property
 	public function __get($property)
 	{
 		return is_null($this->$property) ? null : $this->$property;
-		return $this->property;
+	}
+	public function fakeAttributes()
+	{
+		return $this->renderAttributes();
 	}
 }

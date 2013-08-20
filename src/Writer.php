663b662bb3b9e27e2e98589ee3273d68014c2512
micah eschbacher <michaeleschbacher@gmail.com>
2013-08-20 12:40:53 -0500

    Set up the contstructor to have a root element

--- src/Writer.php
+++ src/Writer.php
@@ -3,7 +3,12 @@ namespace exemel;
 
 class Writer 
 {
+	protected $root;
 
+	public function __construct($root_title = 'root')
+	{
+		$this->root = new Property($root_title);
+	}
 }
 
 
--- tests/WriterTest.php
+++ tests/WriterTest.php
@@ -1,16 +1,37 @@
 <?php
-
 require 'src/Writer.php';
+require 'src/Property.php';
+
+use \exemel;
 class WriterTest extends PHPUnit_Framework_TestCase
 {
-//	public function testConstructorWithParams()
-//	{
-//		$xml = new \eXeMeL\Writer('root', );
-//		$this->fail('asdf');
-//	}
-
-	public function testContructorWithObject()
+	public function testConstructorDefaults()
 	{
-		$this->fail('asdf');
+		$xml = new MockWriter;
+	
+		$this->assertInstanceOf("exemel\Property", $xml->root);
+		$this->assertEquals('root', $xml->root->getTitle());
+	}
+	
+	public function testConstructorWithParam()
+	{
+		$root_title = 'product';
+		$xml = new MockWriter($root_title);
+		$this->assertInstanceOf("exemel\Property", $xml->root);
+		$this->assertEquals($root_title, $xml->root->getTitle());
 	}
 }
+
+class MockWriter extends exemel\Writer
+{
+	public function __get($val)
+	{
+		return $this->$val;
+	}
+	public function __set($key, $val)
+	{
+		$this->$key = $val;
+	}
+} 
+
+
